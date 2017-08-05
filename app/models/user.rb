c33ad107b07201c33e17124be08da48156876a6a
Snigdha Sur <Snigdha@Snigdhas-MacBook-Air-2.local>
2017-08-05 16:36:26 -0400

    updated associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,36 +15,5 @@ class User < ActiveRecord::Base
 
 
 
-
-  # has_many :guests, through: :trips, source: :guest
-  # has_many :hosts, through: :listings, source: :host
-
-
-  # def host_reviews
-  # 	if self.host == true 
-  # 		host_listings = Listing.all.where(host_id: self.id)
-  # 		review_array = []
-  # 		host_listings.each do |listing|
-  # 			review_array << listing.reviews   		
-  # 		end
-  # 		review_array.flatten 
-  # 	end
-  # end 
-
-  # def guests
-  # 	if self.host == true 
-  # 		host_listings = Listing.all.where(host_id: self.id)
-  # 		binding.pry
-  # 		review_array = []
-  # 		host_listings.each do |listing|
-  # 			review_array << listing.reviews   		
-
-  # 		end
-  # 		review_array.flatten 
-  # 	end
-  # end  
-
-
-
   
 end
